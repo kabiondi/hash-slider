5f7c63127ad3b054571480d7b10a779f385996fe
kabiondi <kellenbiondi@gmail.com>
2015-04-20 22:38:34 -0700

    hamburger toggle active works

--- js/controllers/home-controller.js
+++ js/controllers/home-controller.js
@@ -2,11 +2,26 @@
 	
 	app.controller('homeCtrl', ['$scope', 'hashFactory', function ($scope, hashFactory) {
 		var tag = hashFactory.setHashtag();
+		var hamToggle = document.getElementsByClassName("hamburger")[0];
+
 
 		$scope.submitTag = function () {
 			tag = $scope.inputHashtag;
 			hashFactory.setHashtag(tag);
 		}
+
+		function toggleHandler(toggle) {
+			toggle.addEventListener( "click", function(e) {
+				e.preventDefault();
+				if (this.classList.contains("active") === true) {
+					this.classList.remove("active");
+				}
+				else {
+					this.classList.add("active");
+				}
+			});
+		}
+		toggleHandler(hamToggle);
 
 	}]);
 
